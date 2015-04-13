d637b3b0a0a17c85610d34359cd7b5387b9cd089
drednought08 <drednought008@gmail.com>
2015-04-12 22:12:59 -0500

    Create Bus.java
    A very blank slate before until I know what events trigger what

---
+++ Bus.java
@@ -0,0 +1,174 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+
+public class Bus 
+{
+	public Bus()
+	{
+		
+	}
+	
+	class MouseClick implements MouseListener
+	{
+		public void mousePressed( MouseEvent event )
+	      {  
+	         
+	      }
+
+	      // Do-nothing methods
+	      public void mouseReleased( MouseEvent event ) {}
+	      public void mouseClicked( MouseEvent event ) {}
+	      public void mouseEntered( MouseEvent event ) {}
+	      public void mouseExited( MouseEvent event ) {}
+		
+	}
+	
+	class WKeyListener implements KeyListener
+	{
+		public void keyPressed( KeyEvent event ) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void keyReleased( KeyEvent event ) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void keyTyped( KeyEvent event ) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+	
+	class AKeyListener implements KeyListener
+	{
+		public void keyPressed( KeyEvent event ) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void keyReleased( KeyEvent event ) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void keyTyped( KeyEvent event ) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+	
+	class SKeyListener implements KeyListener
+	{
+		public void keyPressed( KeyEvent event ) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void keyReleased( KeyEvent event ) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void keyTyped( KeyEvent event ) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+	
+	class DKeyListener implements KeyListener
+	{
+		public void keyPressed( KeyEvent event ) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void keyReleased( KeyEvent event ) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void keyTyped( KeyEvent event ) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+	
+	class UpArrowListener implements KeyListener
+	{
+		public void keyPressed( KeyEvent event ) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void keyReleased( KeyEvent event ) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void keyTyped( KeyEvent event ) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+	
+	class LeftArrowListener implements KeyListener
+	{
+		public void keyPressed( KeyEvent event ) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void keyReleased( KeyEvent event ) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void keyTyped( KeyEvent event ) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+	
+	class DownArrowListener implements KeyListener
+	{
+		public void keyPressed( KeyEvent event ) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void keyReleased( KeyEvent event ) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void keyTyped( KeyEvent event ) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+	
+	class RightArrowListener implements KeyListener
+	{
+		public void keyPressed( KeyEvent event ) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void keyReleased( KeyEvent event ) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void keyTyped( KeyEvent event ) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+}
