eae95b155bcc37308de0b539564885e72ce1ca27
drednought08 <drednought008@gmail.com>
2015-04-25 15:19:17 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -4,14 +4,16 @@ public class Player
   private int power;
   private int endurance;
   private int speed;
+  private int defense;
   private boolean protect;
   
-  public Player( int heal, int pow, int end, int sp )
+  public Player( int heal, int pow, int end, int sp, int def )
   {
       health = heal;
       power = pow;
       endurance = end;
       speed = sp;
+      defense = def;
       protect = false;
   }
   public int getHealth()
@@ -52,6 +54,16 @@ public class Player
 	public void setSpeed( int sp )
 	{
 	    speed = sp;
+	}
+	
+	public int getDefense()
+	{
+		return defense;
+	}
+	
+	public void setDefense( int def )
+	{
+		defense = def;
 	}
 	
 	public boolean getProtect()
