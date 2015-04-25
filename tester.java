de48d2d7b925ce9b44315ee6d169f4e37e501bb7
drednought08 <drednought008@gmail.com>
2015-04-24 21:01:26 -0500

    Update tester.java

--- tester.java
+++ tester.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
+
 public class tester
+{
+      public static void main( String[] args )
+      {
+            Scanner input = new Scanner( System.in );
+        
+            Player player1 = new Player( 100, 20, 10, 10 );
+            Player player2 = new Player( 100, 20, 10, 10 );
+            
+            Battle newBattle = new Battle();
+            
+            newBattle.battleStart( player1, player2 );
+      }
+}
