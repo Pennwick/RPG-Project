b479334f0f2305a7ee8273c5b41a54665bab21a6
drednought08 <drednought008@gmail.com>
2015-04-24 19:45:05 -0500

    Update Battle.java

--- Battle.java
+++ Battle.java
@@ -1,30 +1,71 @@
-import java.util.Scanner;
-
 public class Battle
 {
 	
-	public void attack( Object playerA, Object playerB )
+	public void attack( Object playerA, Object playerB, int defense )
 	{
-	    	int damage = ( playerA.getPower() - playerB.getEndurance() );
-	    
-	    	playerB.getHealth() -= damage;
-	    
-	    	if ( playerB.getHealth() <= 0 )
+		// playerA is attacking playerB
+		int damage;
+		
+		if ( playerB.getProtect() == true )
+		{
+			damage = ( playerA.getPower() - ( playerB.getEndurance() + defense ) );
+		}
+		else
+		{
+	    		damage = ( playerA.getPower() - playerB.getEndurance() );
+		}
+		
+	    	if ( damage > 0 )
 	    	{
-	        	victory( playerA );
+	    		playerB.setHealth( playerB.getHealth() -= damage );
+	    
+	    		if ( playerB.getHealth() <= 0 )
+	    		{
+	        		victory( playerA );
+	    		}
 	    	}
 	}
 	
-	public void defend( Object player, int defense )
+	public void defend( Object player )
 	{
-	    	player.getEndurance() += defense;
-	    	player.protected = true;
+	    	player.setProtect( true );
 	}
 	
 	public void victory( Object player )
 	{
 	    
 	}
+	
+	public void battleStart( Object playerA, Object playerB )
+	{
+		startTurn( 1, playerA, playerB );
+	}
+	
+	public void startTurn( int turnNumber, Object playerA, Object playerB )
+	{
+		//start the turn and determine the order
+		int turn = turnNumber;
+		
+		playerA.setProtect( false )
+		
+		if ( playerA.getSpeed() > playerB.getSpeed() )
+		{
+			int pA = 1;
+			int pB = 2;
+		}
+		
+		if ( playerB.getSpeed() > playerA.getSpeed() )
+		{
+			int pB = 1;
+			int pA = 2;
+		}
+		
+		if ( playerA.getSpeed() == playerB.getSPeed() )
+		{
+			//randomly pick turn order
+		}
+	}
+	
 	
 	public static void main (String[] args)
 	   {
