9c7fcf482d25d7b60374ce36741c0aa05dfc5184
drednought08 <drednought008@gmail.com>
2015-04-28 18:56:24 -0500

    Update tester.java

--- tester.java
+++ tester.java
@@ -6,8 +6,8 @@ public class tester
       {
             Scanner input = new Scanner( System.in );
         
-            Player player1 = new Player( 100, 20, 10, 10 );
-            Player player2 = new Player( 100, 20, 10, 10 );
+            Player player1 = new Player( 100, 20, 10, 10, 5 );
+            Player player2 = new Player( 100, 20, 10, 10, 5 );
             
             int choice1 = 0;
             int choice2 = 0;
@@ -15,18 +15,34 @@ public class tester
             Battle newBattle = new Battle();
             
             boolean gameLoop = true
-            
-            newBattle.battleStart( player1, player2 );
+            int turn = 1;
             
             while ( gameLoop )
             {
+                  newBattle.startTurn( turn, player1, player2 )
+                  
                   System.out.println( "Player 1 enter 1 to attack, 2 to defend, or 0 to quit: " );
                   choice1 = input.nextInt();
-                  //newBattle.actionSelect( input.nextInt(), newBattle.getFirst(), newBattle.getSecond() );
                   
                   System.out.println( "Player 2 enter 1 to attack, 2 to defend, or 0 to quit: " );
                   choice2 = input.nextInt();
-                  //newBattle.actionSelect( input.nextInt(), newBattle.getSecond(), newBattle.getFirst() );
+                  
+                  if ( choice1 == 0 && choice2 == 0 )
+                  {
+                        break;
+                  }
+                  else if ( choice2 == 2 )
+                  {
+                        newBattle.actionSelect( choice2, newBattle.getSecond(), newBattle.getFirst() );
+                        newBattle.actionSelect( choice1, newBattle.getFirst(), newBattle.getSecond() );
+                  }
+                  else if ( choice2 == 1 )
+                  {
+                        newBattle.actionSelect( choice1, newBattle.getFirst(), newBattle.getSecond() );
+                        newBattle.actionSelect( choice2, newBattle.getSecond(), newBattle.getFirst() );
+                  }
+                  
+                  turn += 1;
             }
       }
 }
