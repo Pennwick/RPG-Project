e164c52458dfecfde69622332c92b0cb1d2b8de6
drednought08 <drednought008@gmail.com>
2015-04-30 13:58:08 -0500

    Update tester.java

--- tester.java
+++ tester.java
@@ -6,15 +6,16 @@ public class tester
       {
             Scanner input = new Scanner( System.in );
         
-            Player player1 = new Player( 100, 20, 10, 10, 5 );
-            Player player2 = new Player( 100, 20, 10, 8, 5 );
+            Player player1 = new Player( 100, 20, 10, 10 );
+            Player player2 = new Player( 100, 20, 10, 10 );
             
             int choice1 = 0;
             int choice2 = 0;
             
             Battle newBattle = new Battle();
+            Training newTrainer = new Training();
             
-            boolean gameLoop = true
+            boolean gameLoop = true;
             
             System.out.println( "Player 1 you have 10 days to train." );
             
@@ -26,10 +27,12 @@ public class tester
                   
                   switch( choice1 )
                   {
-                        case 1: trainPower( player1 ); System.out.println( "Power raised to " + player1.getPower() ); break;
-                        case 2: trainEndurance( player1 ); System.out.println( "Endurance raised to " + player1.getEndurance() ); break;
+                        case 1: newTrainer.trainPower( player1 ); System.out.println( "Power raised to " + player1.getPower() ); break;
+                        case 2: newTrainer.trainEndurance( player1 ); System.out.println( "Endurance raised to " + player1.getEndurance() ); break;
                   }
             }
+            
+            System.out.println( "Player 1: Health: " + player1.getMaxHealth() + " Power: " + player1.getPower() + " Endurance: " + player1.getEndurance() + " Speed: " + player1.getSpeed() );
             
             System.out.println( "Player 2 you have 10 days to train." );
             
@@ -41,14 +44,16 @@ public class tester
                   
                   switch( choice1 )
                   {
-                        case 1: trainPower( player2 ); System.out.println( "Power raised to " + player2.getPower() ); break;
-                        case 2: trainEndurance( player2 ); System.out.println( "Endurance raised to " + player2.getEndurance() ); break;
+                        case 1: newTrainer.trainPower( player2 ); System.out.println( "Power raised to " + player2.getPower() ); break;
+                        case 2: newTrainer.trainEndurance( player2 ); System.out.println( "Endurance raised to " + player2.getEndurance() ); break;
                   }
             }
             
+            System.out.println( "Player 2: Health: " + player2.getMaxHealth() + " Power: " + player2.getPower() + " Endurance: " + player2.getEndurance() + " Speed: " + player2.getSpeed() );
+            
             while ( gameLoop )
             {
-                  newBattle.startTurn( player1, player2 )
+                  newBattle.startTurn( player1, player2 );
                   
                   System.out.println( "Player 1 enter 1 to attack, 2 to defend, or 0 to quit: " );
                   choice1 = input.nextInt();
@@ -70,6 +75,13 @@ public class tester
                         newBattle.actionSelect( choice1, newBattle.getFirst(), newBattle.getSecond() );
                         newBattle.actionSelect( choice2, newBattle.getSecond(), newBattle.getFirst() );
                   }
+                  
+                  if ( newBattle.checkVictory( player1, player2 ) )
+                  {
+                	  gameLoop = false;
+                  }
             }
+            
+            input.close();
       }
 }
