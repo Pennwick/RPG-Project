101b8631786edfea220c48eca5fa6be58264bed0
drednought08 <drednought008@gmail.com>
2015-05-05 00:22:09 -0500

    Create frameTester.java

---
+++ frameTester.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class frameTester 
+{
+	public static void main( String[] args )
+	{
+		JFrame frame = new Frame();
+		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		frame.setTitle( "RPG" );
+		frame.setVisible( true );
+	}
+}
